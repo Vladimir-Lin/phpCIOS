04856be9f47a94e4cd27e410500ec03ae9ffb153
foxman <lin.vladimir@gmail.com>
2019-09-12 21:23:07 +0800

    add glosbe

--- src/Glosbe.php
+++ src/Glosbe.php
@@ -12,6 +12,8 @@ public $Dest    ;
 public $Format  ;
 public $Tm      ;
 public $Pretty  ;
+public $Results ;
+public $JSON    ;
 
 /////////////////////////////////////////////////////////
 
@@ -36,21 +38,39 @@ public function Clear ( )
 
 public function Query ( $Phrase )
 {
-  $PAVH = $this -> Path                                               ;
-  $PSIZ = $this -> Page                                               ;
-  $KS   = rawurlencode ( $KEY )                                       ;
-  $PAXH = "{$PAVH}per_page={$PSIZ}&query='{$KS}'"                     ;
-  $ch   = curl_init  (                                              ) ;
-  curl_setopt        ( $ch , CURLOPT_URL            , $PAXH         ) ;
-  curl_setopt        ( $ch , CURLOPT_HTTPHEADER     , $this -> Auth ) ;
-  curl_setopt        ( $ch , CURLOPT_RETURNTRANSFER , 1             ) ;
-  curl_setopt        ( $ch , CURLOPT_SSL_VERIFYHOST , 0             ) ;
-  curl_setopt        ( $ch , CURLOPT_SSL_VERIFYPEER , 0             ) ;
-  $temp = curl_exec  ( $ch                                          ) ;
-          curl_close ( $ch                                          ) ;
-  $this -> Results = $temp                                            ;
-//  $this -> JSON    = json_decode ( $temp                            ) ;
-  return $this -> Results                                             ;
+  $PAVH   = $this -> Path                                                    ;
+  $FPAT   = "from={$this->From}"                                             ;
+  $PAXH   = "{$PAVH}{$FPAT}"                                                 ;
+  $DEST   = "dest={$this->Dest}"                                             ;
+  $PAXH   = "{$PAVH}&{$DEST}"                                                ;
+  $FMTX   = "format={$this->Format}"                                         ;
+  $PAXH   = "{$PAVH}&{$FMTX}"                                                ;
+  if ( $this -> Tm )                                                         {
+    $TMVX = "tm=true"                                                        ;
+  } else                                                                     {
+    $TMVX = "tm=false"                                                       ;
+  }                                                                          ;
+  $PAXH   = "{$PAVH}&{$TMVX}"                                                ;
+  if ( $this -> Pretty )                                                     {
+    $PTVX = "pretty=true"                                                    ;
+  } else                                                                     {
+    $PTVX = "pretty=false"                                                   ;
+  }                                                                          ;
+  $PAXH   = "{$PAVH}&page=1&pageSize=30"                                     ;
+  $PAXH   = "{$PAVH}&{$PTVX}"                                                ;
+  $KS   = rawurlencode ( $Phrase )                                           ;
+  $PAXH = "{$PAVH}&phrase={$KS}"                                             ;
+  $ch   = curl_init  (                                                     ) ;
+  curl_setopt        ( $ch , CURLOPT_URL            , $PAXH                ) ;
+  curl_setopt        ( $ch , CURLOPT_RETURNTRANSFER , 1                    ) ;
+  curl_setopt        ( $ch , CURLOPT_ENCODING       , ''                   ) ;
+//  curl_setopt        ( $ch , CURLOPT_SSL_VERIFYHOST , 0             )        ;
+//  curl_setopt        ( $ch , CURLOPT_SSL_VERIFYPEER , 0             )        ;
+  $temp = curl_exec  ( $ch                                                 ) ;
+          curl_close ( $ch                                                 ) ;
+  $this -> Results = $temp                                                   ;
+  $this -> JSON    = json_decode ( $temp                                   ) ;
+  return $this -> Results                                                    ;
 }
 
 }
