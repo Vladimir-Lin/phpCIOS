a292de1d803cef3b6020ea89bb1badd584ead82a
foxman <lin.vladimir@gmail.com>
2020-06-20 18:48:07 +0800

    Commit CIOS For PHP

--- src/OpenFire.php
+++ src/OpenFire.php
@@ -1,56 +1,48 @@
 <?php
-
-namespace CIOS ;
-use Gnello\OpenFireRestAPI\Client as Client ;
-
-class OpenFire
-{
-
-public $client   ;
-public $Database ;
-public $Response ;
-
-function __construct()
-{
-  $this -> Clear ( ) ;
+//////////////////////////////////////////////////////////////////////////////
+namespace CIOS                                                               ;
+use Gnello\OpenFireRestAPI\Client as Client                                  ;
+//////////////////////////////////////////////////////////////////////////////
+class OpenFire                                                               {
+//////////////////////////////////////////////////////////////////////////////
+public $client                                                               ;
+public $Database                                                             ;
+public $Response                                                             ;
+//////////////////////////////////////////////////////////////////////////////
+function __construct ( )                                                     {
+  $this -> Clear ( )                                                         ;
 }
-
-function __destruct()
-{
+//////////////////////////////////////////////////////////////////////////////
+function __destruct ( )                                                      {
 }
-
-public function Clear ( )
-{
-  $this -> Database = "openfire" ;
+//////////////////////////////////////////////////////////////////////////////
+public function Clear ( )                                                    {
+  $this -> Database = "openfire"                                             ;
 }
-
-public function Table ( $table )
-{
-  $DBT = $this -> Database     ;
-  return "`{$DBT}`.`{$table}`" ;
+//////////////////////////////////////////////////////////////////////////////
+public function Table ( $table )                                             {
+  $DBT = $this -> Database                                                   ;
+  return "`{$DBT}`.`{$table}`"                                               ;
 }
-
-public function loginAdmin ( $account )
-{
+//////////////////////////////////////////////////////////////////////////////
+public function loginAdmin ( $account )                                      {
   $this -> client = new Client( [ 'client' => $account , 'guzzle' => [ ] ] ) ;
 }
-
-public function Protocol ( )
-{
+//////////////////////////////////////////////////////////////////////////////
+public function Protocol ( )                                                 {
   $PROTOCOL   = "http"                                                       ;
   if ( isset($_SERVER['HTTPS']) && ( $_SERVER['HTTPS'] === 'on' ) )          {
     $PROTOCOL = "https"                                                      ;
   }                                                                          ;
   return $PROTOCOL                                                           ;
 }
-
-public function Password ( $DB , $user )
-{
+//////////////////////////////////////////////////////////////////////////////
+public function Password ( $DB , $user )                                     {
   $TBL = $this -> Table  ( "ofuser" )                                        ;
   $QQ = "select `plainPassword` from {$TBL} where `username` = '{$user}' ;"  ;
   return $DB -> FetchOne ( $QQ      )                                        ;
 }
-
+//////////////////////////////////////////////////////////////////////////////
 public function Create ( $username                                           ,
                          $password                                           ,
                          $name  = ""                                         ,
@@ -66,7 +58,7 @@ public function Create ( $username                                           ,
   if ( strlen ( $email ) > 0 ) $CUA [ "email" ] = $email                     ;
   return $this -> CreateUser ( $CUA , $group )                               ;
 }
-
+//////////////////////////////////////////////////////////////////////////////
 public function CreateUser ( $user , $group = "" )
 {
   $RESP = $this -> client -> getUserModel ( ) -> createUser ( $user )        ;
@@ -79,19 +71,17 @@ public function CreateUser ( $user , $group = "" )
   }                                                                          ;
   return true                                                                ;
 }
-
-public function JoinGroup ( $user , $group )
-{
-  if ( strlen ( $group ) <= 0 ) return false    ;
-  $RESP = $this -> client  -> getUserModel ( ) ->
-          addUserToGroup ( $user , $group )     ;
-  $this -> Response = $RESP                     ;
-  if ( $RESP -> getStatusCode ( ) == 201 )      {
-    return true                                 ;
-  }                                             ;
-  return false                                  ;
+//////////////////////////////////////////////////////////////////////////////
+public function JoinGroup ( $user , $group )                                 {
+  if ( strlen ( $group ) <= 0 ) return false                                 ;
+  $RESP = $this -> client -> getUserModel ( ) -> addUserToGroup ( $user , $group ) ;
+  $this -> Response = $RESP                                                  ;
+  if ( $RESP -> getStatusCode ( ) == 201 )                                   {
+    return true                                                              ;
+  }                                                                          ;
+  return false                                                               ;
 }
-
+//////////////////////////////////////////////////////////////////////////////
 }
-
+//////////////////////////////////////////////////////////////////////////////
 ?>
