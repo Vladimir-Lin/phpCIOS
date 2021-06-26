5c7c28641996f582353e40adaf5f36e72075f5ba
foxman <lin.vladimir@gmail.com>
2021-06-26 20:31:53 +0800

    Commit CIOS For PHP

--- src/SmsEvery8d.php
+++ src/SmsEvery8d.php
@@ -27,6 +27,7 @@ function Initialize   (                                                    ) {
   $this -> Cust           = ""                                               ;
   $this -> CurrentCredits = 0                                                ;
   $this -> CurrentError   = ""                                               ;
+  $this -> XML            = null                                             ;
   ////////////////////////////////////////////////////////////////////////////
   if                  ( ! array_key_exists ( $KEY , $GLOBALS )             ) {
     return false                                                             ;
@@ -48,6 +49,35 @@ function management   (                                                    ) {
 //////////////////////////////////////////////////////////////////////////////
 function login        (                                                    ) {
   ////////////////////////////////////////////////////////////////////////////
+  // 登入Every8d帳戶
+  ////////////////////////////////////////////////////////////////////////////
+  $this -> CurrentError = ""                                                 ;
+  ////////////////////////////////////////////////////////////////////////////
+  $PARAMS = array                                                            (
+    "custID"   => $this -> Cust                                              ,
+    "userID"   => $this -> Username                                          ,
+    "password" => $this -> Password                                          ,
+    "APIType"  => ""                                                         ,
+    "version"  => ""                                                       ) ;
+  ////////////////////////////////////////////////////////////////////////////
+  try                                                                        {
+    $CLIENT      = new \SoapClient      ( $this -> LoginURL )                ;
+    $RESULT      = $CLIENT -> Login     ( $PARAMS           )                ;
+    $XMLSTR      = $RESULT -> LoginResult                                    ;
+    //////////////////////////////////////////////////////////////////////////
+    // 取得登入結果
+    //////////////////////////////////////////////////////////////////////////
+    $this -> XML = new \SimpleXMLElement ( $XMLSTR           )               ;
+    if ( $this -> XML -> ERROR_CODE == "0000" )                              {
+      $this -> CurrentCredits = $this -> XML -> CREDIT                       ;
+      return true                                                            ;
+    } else                                                                   {
+      $this -> CurrentError = (string) $this -> XML -> DESC [ 0 ]            ;
+    }                                                                        ;
+  } catch ( Exception $e )                                                   {
+    $this -> CurrentError = "Failure to create a SOAP client for Every8d"    ;
+    return false                                                             ;
+  }                                                                          ;
   ////////////////////////////////////////////////////////////////////////////
   return false                                                               ;
 }
@@ -57,15 +87,49 @@ function error        (                                                    ) {
 }
 //////////////////////////////////////////////////////////////////////////////
 function credits      (                                                    ) {
-  ////////////////////////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////
-  return false                                                               ;
+  return $this -> CurrentCredits                                             ;
 }
 //////////////////////////////////////////////////////////////////////////////
 function send         ( $Phone , $Content , $Title = ""                    ) {
   ////////////////////////////////////////////////////////////////////////////
+  $this -> CurrentError = ""                                                 ;
   ////////////////////////////////////////////////////////////////////////////
-  return false                                                               ;
+  $UserNo    = $this -> XML -> USER_NO                                       ; // 從登入結果取得UserNo
+  $CompanyNo = $this -> XML -> COMPANY_NO                                    ; // 從登入結果取得Company_No
+  $Credit    = $this -> XML -> CREDIT                                        ; // 從登入結果取得目前剩餘額度
+  ////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////
+  $SML  = '<REPS VER="2.0"><IP></IP><CARD_NO/>'                              ;
+  $SML .= '<USER NAME="" '                                                   ;
+  $SML .= 'MOBILE="' . $Phone . '" '                                         ; // 電話號碼
+  $SML .= 'EMAIL="" SENDTIME="" PARAM="" MR=""/>'                            ;
+  $SML .= '</REPS>'                                                          ;
+  ////////////////////////////////////////////////////////////////////////////
+  $PARAMS = array                                                            (
+    "custID"      => $this -> Cust                                           ,
+    "CompanyNo"   => $CompanyNo                                              ,
+    "userNo"      => $UserNo                                                 ,
+    "sendtype"    => "110"                                                   ,
+    "msgCategory" => "10"                                                    ,
+    "subject"     => ""                                                      ,
+    "content"     => $Content                                                ,
+    "image"       => ""                                                      ,
+    "Audio"       => ""                                                      ,
+    "xml"         => $SML                                                    ,
+    "batchID"     => ""                                                      ,
+    "certified"   => ""                                                    ) ;
+  ////////////////////////////////////////////////////////////////////////////
+  // 發送簡訊
+  ////////////////////////////////////////////////////////////////////////////
+  $XMS    = new \SoapClient    ( $this -> URL )                              ;
+  $RESULT = $XMS    -> QueueIn ( $PARAMS      )                              ;
+  $STR    = $RESULT -> QueueInResult                                         ;
+  if ( substr ( $STR , 0 , 1 ) == "-" )                                      {
+    $this -> CurrentError = "Failure to send message to phone {$Phone}"      ;
+    return false                                                             ;
+  }                                                                          ;
+  ////////////////////////////////////////////////////////////////////////////
+  return true                                                                ;
 }
 //////////////////////////////////////////////////////////////////////////////
 }
